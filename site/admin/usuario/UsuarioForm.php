099f93319656bb91a554db75120c5b10a05fe173
zanotelliph <pedrozanotelli5@gmail.com>
2025-11-19 10:08:43 -0300

    Commit

--- site/admin/usuario/UsuarioForm.php
+++ site/admin/usuario/UsuarioForm.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/UsuarioControle.php';
 require_once __DIR__ . '/../admin/header.php';
 session_start();
