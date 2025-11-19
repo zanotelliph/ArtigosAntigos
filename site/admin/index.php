8f93cf9efb32a7052d29616907d44eff0ab6a7d8
zanotelliph <pedrozanotelli5@gmail.com>
2025-11-19 10:46:50 -0300

    Outra commit

--- site/admin/index.php
+++ site/admin/index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once __DIR__ . '/db.class.php';
+require_once __DIR__ . '/site/database/db.class.php';
 require_once 'header.php';
 
 redirectIfNotLoggedIn();
